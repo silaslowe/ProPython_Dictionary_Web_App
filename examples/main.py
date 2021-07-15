e59dcd95a1ccd6529e76c1e44e8abaac96148e29
silaslowe@gmail.com <silaslowe@gmail.com>
2021-07-15 17:54:03 -0500

    added container div and styles to example

--- examples/main.py
+++ examples/main.py
@@ -4,11 +4,13 @@ import justpy as jp
 @jp.SetRoute("/home")
 def home():
     wp = jp.WebPage()
-    jp.Div(a=wp, text="Hello Werld!",
-           classes="text-green-700 bg-yellow-500")
-    jp.Div(a=wp, text="Hello Werld Mas!", classes="font-serif text-lg")
+    div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+    jp.Input(a=div, placeholder="input1", classes="form-input")
+    jp.Input(a=div, placeholder="input2", classes="form-input")
+    jp.Div(a=div, text="Result...", classes="text-gray-500")
+    jp.Button(a=div, text="Calculate", classes='border border-blue-500 m-2 py-1 px-4 rounded '
+                                               'text-blue-600 hover:bg-red-500 hover:text-white')
     return wp
-
 
 
 # jp.Route("/", home)
