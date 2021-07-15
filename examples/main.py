83d6b81ca501cc18046f9beeb4b82e791b20d414
silaslowe@gmail.com <silaslowe@gmail.com>
2021-07-15 14:56:56 -0500

    added tailwind styles

--- examples/main.py
+++ examples/main.py
@@ -4,8 +4,9 @@ import justpy as jp
 @jp.SetRoute("/home")
 def home():
     wp = jp.WebPage()
-    jp.Div(a=wp, text="Hello Werld!")
-    jp.Div(a=wp, text="Hello Werld Mas!")
+    jp.Div(a=wp, text="Hello Werld!",
+           classes="text-green-700 bg-yellow-500")
+    jp.Div(a=wp, text="Hello Werld Mas!", classes="font-serif text-lg")
     return wp
 
 @jp.SetRoute("/about")
