e8c991bd59f99d1518cf279e39a10eca8d45aa3c
silaslowe@gmail.com <silaslowe@gmail.com>
2021-07-16 09:16:07 -0500

    added home and main pages

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import justpy as jp
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from webapp.about import About
+from webapp.home import Home
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+jp.Route(Home.path, Home.serve)
+jp.Route(About.path, About.serve)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+jp.justpy(port=8001)
